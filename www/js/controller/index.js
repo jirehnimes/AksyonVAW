0ada0f51a63363460c4d775bd97401b73c344ad6
Jireh June Nimes <jjnimes@gmail.com>
2016-08-31 21:41:26 +0800

    Added plugins

---
+++ www/js/controller/index.js
@@ -0,0 +1,6 @@
+angular.module('aksyonvaw.indexCtrl', [])
+
+.controller('IndexCtrl', function($scope, $state) {
+
+});
+
---
+++ www/js/route.js
@@ -0,0 +1,46 @@
+angular.module('aksyonvaw.routes', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+    .state('index', {
+      url: '/',
+      templateUrl: 'view/index.html',
+      controller: 'IndexCtrl'
+    })
+
+
+
+    // .state('menu', {
+    //   url: '/',
+    //   templateUrl: 'view/menu.html',
+    //   controller: 'MenuCtrl'
+    // })
+
+
+
+    // .state('menu.home', {
+    //   url: 'home',
+    //   views: {
+    //     'menuContent': {
+    //       templateUrl: 'view/home.html',
+    //       // controller: 'HomeCtrl'
+    //     }
+    //   }
+    // })
+    
+
+    
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+
+});
